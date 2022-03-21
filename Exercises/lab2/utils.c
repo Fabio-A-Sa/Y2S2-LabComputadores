7fb135bd14dd27ec4df785f475a7cd6ddb244382
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-21 20:38:06 +0000

    utils

--- Exercises/lab2/utils.c
+++ Exercises/lab2/utils.c
@@ -4,21 +4,20 @@
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
 
-  *lsb = 0xFF & val;
-  return 1;
+  *lsb = 0xFF & val;                    // Coloca em LSB os 8 bits menos significativos
+  return 0;                             // Retorna se houve algum erro (0 -> false)
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
 
-  *msb = (val >> 8) & 0xFF;
-  return 1;
+  *msb = (val >> 8) & 0xFF;             // Coloca em MSB os 8 bits mais significativos
+  return 0;                             // Retorna se houve algum erro (0 -> false)
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  uint32_t val;
-  int ret = sys_inb(port, &val);
 
-  *value = 0xFF & val;
-
-  return ret;
+  uint32_t val;                         // instanciar uma variável: unsigned int de 32 bits
+  int ret = sys_inb(port, &val);        // System call que irá colocar o conteúdo de port na zona de memória de val
+  *value = 0xFF & val;                  // value ficará com os 8 bits menos significativos, ou seja, a control word do timer
+  return ret;                           // Retorna se houve algum erro (0 -> false, 1 -> true)
 }
