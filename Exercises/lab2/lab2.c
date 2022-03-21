6f1579979c859a63fbbaad40043ed758d735d896
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-21 22:28:52 +0000

    drafts

--- Exercises/lab2/lab2.c
+++ Exercises/lab2/lab2.c
@@ -31,7 +31,11 @@ int main(int argc, char *argv[]) {
 
 int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
 
-  return 1;
+  uint8_t configuration;                                    // variável que vai conter a configuração do timer
+  int ret = timer_get_config(timer, &configuration);q       // chamar a função que preenche a configuração
+  if (ret != 0) return ret;                                 // se houve erro, abortar logo a missão
+
+  return timer_display_conf(timer, configuration, field);   // display das cofigurações segundo o field
 }
 
 int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
