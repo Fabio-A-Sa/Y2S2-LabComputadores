899ed9e8d6421d696834f1d0450e94c8090e2ae0
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-26 18:41:25 +0000

    utils

--- Exercises/lab2/utils.c
+++ Exercises/lab2/utils.c
@@ -1,12 +1,11 @@
 #include <lcom/lcf.h>
-
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
 
   if (lsb == NULL) return 1;
 
-  *lsb = 0xFF & val;
+  *lsb = 0xFF & val;              // retorna os 8 bits menos significativos
   return 0;
 }
 
@@ -14,7 +13,7 @@ int(util_get_MSB)(uint16_t val, uint8_t *msb) {
 
   if (msb == NULL) return 1;
 
-  *msb = (val >> 8) & 0xFF;
+  *msb = (val >> 8) & 0xFF;       // retorna os 8 bits mais significativos
   return 0;
 }
 
@@ -24,6 +23,6 @@ int (util_sys_inb)(int port, uint8_t *value) {
 
   uint32_t val;
   int ret = sys_inb(port, &val);
-  *value = 0xFF & val;
-  return ret;
+  *value = 0xFF & val;            // coloca em *value os 8 bits menos significativos do retorno
+  return ret;                     // do timer port (0x4{0, 1, 2}), ou seja, a configuração ativa
 }
