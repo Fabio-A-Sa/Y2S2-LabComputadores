2ea1f0749e8cd6b05c48148f719dc5b0afb02362
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-29 01:44:15 +0100

    important macros content

--- Exercises/lab3/i8042.h
+++ Exercises/lab3/i8042.h
@@ -1,33 +1,24 @@
 #ifndef I8042_H
 #define I8042_H
 
-#define KBD_IRQ  1
+#define BIT(n)          (0x01 << (n))
 
-#define ESC_BREAK 		0x81
+#define IRQ_KBD         1
 
-#define STAT_REG 		0x64
+#define ESC		        0x81
+
+#define TIMEOUT_ERR     BIT(6)
+#define PARITY_ERR      BIT(7)
+
+#define KBC_ST_REG      0x64
 #define KBC_CMD_REG     0x64
-#define IN_BUF		    0x64
-#define OUT_BUF 		0x60
-#define OUT_BUF_FULL 0x01
+#define KBC_OUT_BUF     0x60
 
-#define BIT(n)    (0x01<<(n))
-#define TWO_BYTES 		0xe0
+#define TWO_BYTES 		0xE0
 
-#define OBF            BIT(0)
-#define IBF            BIT(1)
+#define OBF             BIT(0)
+#define IBF             BIT(1)
 
 #define WAIT_KBC 		20000
-
-#define PAR_ERR BIT(7)
-#define TO_ERR  BIT(6)
-
-#define LEDS_CMD 		0xED
-#define ACK 			0xFA
-#define RESEND 			0xFE
-#define ERROR 			0xFC
-
-#define TRIES 			5
-
 
 #endif /* I8042_H */
