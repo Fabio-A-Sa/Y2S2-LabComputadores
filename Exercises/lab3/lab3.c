3a689a9ae93b298ffcdc91ca69e2640ecb321dbc
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-29 02:26:44 +0100

    some drafts

--- Exercises/lab3/lab3.c
+++ Exercises/lab3/lab3.c
@@ -5,6 +5,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+extern int counter; // KBC.h counter
+
 int main(int argc, char *argv[]) {
 
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -32,15 +34,55 @@ int main(int argc, char *argv[]) {
 
 int(kbd_test_scan)() {
 
-    
+    if (subscribe_KBC_interrupts() != 0) return 1;
 
-    return 1;
+    uint8_t scancode;
+
+    while(scancode != ESC) { /* Run while ESC key isn't pressed */
+
+        /* Get a request message */
+        if (driver_receive(ANY, &msg, &ipc_status) != 0 ) {
+        printf("driver_receive failed with: %d", r);
+        continue;
+        }
+
+        if (is_ipc_notify(ipc_status)) { /* received notification */
+            switch (_ENDPOINT_P(msg.m_source)) {
+                case HARDWARE: /* hardware interrupt notification */
+                if (msg.m_notify.interrupts &irq_set) { /* subscribed interrupt */
+                    kbc_ih();
+                    if(scancode == KBC_2BYTE_CODE){
+                    bytes[i] = scancode;
+                    i++;
+                    continue;
+                    }
+                    bytes[i] = scancode;
+                    kbd_print_scancode(!(scancode & KBC_MSB_SCNCD),i+1,bytes);
+                    i=0;
+                }
+                break;
+                default:
+                break; /* no other notifications expected: do nothing */
+            }
+        } else { /* received a standard message, not a notification */
+        /* no standard messages expected: do nothing */
+        }
+
+    if (unsubscribe_KBC_interrupts() != 0) return 1;
+
+    return 0;
 }
 
 int(kbd_test_poll)() {
 
+    if (subscribe_KBC_interrupts() != 0) return 1;
 
-    return 1;
+
+
+
+    if (unsubscribe_KBC_interrupts() != 0) return 1;
+
+    return 0;
 }
 
 int(kbd_test_timed_scan)(uint8_t n) {
---
+++ Exercises/lab3/utils.c
@@ -0,0 +1,37 @@
+#include <lcom/lcf.h>
+#include <stdint.h>
+
+#ifdef LAB3
+int counter = 0;
+#endif
+
+int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
+
+  if (lsb == NULL) return 1;
+
+  *lsb = 0xFF & val;
+  return 0;
+}
+
+int(util_get_MSB)(uint16_t val, uint8_t *msb) {
+
+  if (msb == NULL) return 1;
+
+  *msb = (val >> 8) & 0xFF;
+  return 0;
+}
+
+int (util_sys_inb)(int port, uint8_t *value) {
+
+  if (value == NULL) return 1;
+
+  uint32_t val;
+  int ret = sys_inb(port, &val);
+
+  #ifdef LAB3
+  counter++;
+  #endif
+
+  *value = 0xFF & val;
+  return ret;
+}
