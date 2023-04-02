248396cfbf7e830eca6e0452dc5cdeb9db4c500f
Fabio-A-Sa <up202007658@fe.up.pt>
2023-04-02 20:49:50 +0100

    template: 3 devices

--- Template/main.c
+++ Template/main.c
@@ -10,8 +10,8 @@ extern SystemState systemState;
 
 int (main)(int argc, char *argv[]) {
   lcf_set_language("EN-US");
-  lcf_trace_calls("/home/lcom/labs/Template/trace.txt");
-  lcf_log_output("/home/lcom/labs/Template/output.txt");
+  lcf_trace_calls("/home/lcom/labs/Template/debug/trace.txt");
+  lcf_log_output("/home/lcom/labs/Template/debug/output.txt");
   if (lcf_start(argc, argv)) return 1;
   lcf_cleanup();
   return 0;
--- Template/states/states.c
+++ Template/states/states.c
@@ -1,19 +1,32 @@
 #include "states.h"
+#include "controllers/keyboard/KBC.h"
+#include "controllers/mouse/mouse.h"
 
 int timer_counter = 0;
+extern uint8_t scancode;
+extern uint8_t byte_index;
+extern struct packet mouse_packet;
 SystemState systemState = RUNNING;
 
 void update_timer_state() {
     timer_counter++;
     if (timer_counter % 30 == 0) {
         printf("Timer up\n");
+        systemState = EXIT;
     }
 }
 
 void update_keyboard_state() {
-    printf("Keyboard up\n");
+    (kbc_ih)();
+    printf("Keyboard up with scancode = %d\n", scancode);
 }
 
 void update_mouse_state() {
-    printf("Mouse up\n");
+    (mouse_ih)();
+    mouse_sync_bytes();
+    if (byte_index == 3) {
+        byte_index = 0;
+        mouse_bytes_to_packet();
+        printf("Mouse up with delta_x = %d\n", mouse_packet.delta_x);
+    }
 }
