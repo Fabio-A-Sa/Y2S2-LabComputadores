12b172aef6e83242f7f51bbc600d026e2c8b998e
Fabio-A-Sa <up202007658@fe.up.pt>
2023-04-13 21:37:28 +0100

    template: view

--- Template/view/view.c
+++ Template/view/view.c
@@ -4,6 +4,7 @@
 #include "controller/mouse/mouse.h"
 #include "model/model.h"
 
+// Variáveis externas importantes à visualização do modelo e dos seus estados
 uint8_t *main_frame_buffer;
 uint8_t *secondary_frame_buffer;
 uint8_t *drawing_frame_buffer;
@@ -12,6 +13,7 @@ extern vbe_mode_info_t mode_info;
 extern MouseInfo mouse_info;
 MenuState menuState = START;
 
+// Objetos
 extern Sprite *mouse;
 extern Sprite *hand;
 extern Sprite *button1;
@@ -19,6 +21,10 @@ extern Sprite *button2;
 extern Sprite *button3;
 extern Sprite *button4;
 
+// Alocação de memória ao(s) buffer(s)
+// Se houver só um buffer, esse é o principal
+// Se houver double buffering, há um secundário a alocar a mesma quantidade de memória que serve
+// exclusivamente para o desenho
 int set_frame_buffers(uint16_t mode) {
     if (set_frame_buffer(mode, &main_frame_buffer)) return 1;
     frame_buffer_size = mode_info.XResolution * mode_info.YResolution * ((mode_info.BitsPerPixel + 7) / 8);
@@ -31,10 +37,21 @@ int set_frame_buffers(uint16_t mode) {
     return 0;
 }
 
+// Double buffering
+// Cópia para o frame buffer principal do frame construído desde a última atualização
+// Otimização: 
+// como o swap é uma operação muito frequente, é melhor não estar  a calcular frame_buffer_size sempre. 
+// Assim opta-se por uma variável global, que é constante ao longo da execução e calculada 1 vez na linha 30.
+// Poupa-se (frequência * (2 multiplicações + 1 soma + 1 divisão)) operações por cada segundo.
 void swap_buffers() {
     memcpy(main_frame_buffer, secondary_frame_buffer, frame_buffer_size);
 }
 
+// A construção de um novo frame é baseado:
+// - no estado atual do modelo (menuState, mouse_info, mode_info, buttonX->pressed...);
+// - no Algoritmo do Pintor - https://pt.wikipedia.org/wiki/Algoritmo_do_pintor
+// A ideia é colocar no buffer primeiro o plano mais longe do observador (a cor do fundo) e só depois 
+// os objetos em cima, no caso do cursor e / ou dos botões
 void draw_new_frame() {
     switch (menuState) {
         case START:
@@ -50,10 +67,12 @@ void draw_new_frame() {
     draw_mouse();
 }
 
+// O menu inicial é apenas um retângulo com tamanho máximo
 void draw_initial_menu() {
     draw_rectangle(0, 0, mode_info.XResolution, mode_info.YResolution, RED, drawing_frame_buffer);
 }
 
+// O menu do jogo é constituído por quatro botões
 void draw_game_menu() {
     draw_sprite_button(button1, 0, 0);
     draw_sprite_button(button2, mode_info.XResolution/2, 0);
@@ -61,10 +80,14 @@ void draw_game_menu() {
     draw_sprite_button(button4, mode_info.XResolution/2, mode_info.YResolution/2);
 }
 
+// O menu final é apenas um retângulo com tamanho máximo
 void draw_finish_menu() {
     draw_rectangle(0, 0, mode_info.XResolution, mode_info.YResolution, DARKBLUE, drawing_frame_buffer);
 }
 
+// O cursor mode ter dois estados:
+// - "normal", quando está no menu de início ou de fim
+// - "mão", quando está no menu com os botões
 void draw_mouse() {
     switch (menuState) {
         case START: case END:
@@ -76,6 +99,9 @@ void draw_mouse() {
     }
 }
 
+// A função recebe um objeto Sprite proveniente de um XPM e mostra-o nas coordenadas (x, y)
+// Usa as cores dinamicamente alocadas na altura da construção
+// A função ignora a cor transparente do XPM para não modificar o fundo quando não é preciso
 int draw_sprite_xpm(Sprite *sprite, int x, int y) { 
     uint16_t height = sprite->height;
     uint16_t width = sprite->width;
@@ -90,6 +116,9 @@ int draw_sprite_xpm(Sprite *sprite, int x, int y) {
     return 0; 
 }
 
+// A função recebe um objeto Sprite de cor constante e mostra-o nas coordenadas (x, y)
+// Usa apenas uma cor, alocada na altura da construção
+// A função ignora a cor transparente do XPM para não modificar o fundo quando não é preciso
 int draw_sprite_button(Sprite *sprite, int x, int y) { 
     uint16_t height = sprite->height;
     uint16_t width = sprite->width;
